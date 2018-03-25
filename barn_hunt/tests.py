230cbcb965b177893f47eaa2ba9e9e283870f8df
Blake Latchford <blake.latchford@gmail.com>
2018-03-25 12:58:45 -0400

    Add Owner model tests.

--- barn_hunt/tests.py
+++ barn_hunt/tests.py
@@ -1,3 +1,25 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Owner
+
+class OwnerTests(TestCase):
+    def test_happy_path(self):
+        owner = Owner(
+            full_name='John Doe',
+            email='john.doe@domain.com')
+        self.assertIsInstance(owner, Owner)
+
+    def test_valid_name_characters(self):
+        valid_name_strings = (
+            "O'Conor",
+        )
+        for name in valid_name_strings:
+            with self.subTest(name=name):
+                owner = Owner(
+                    full_name=name,
+                    email='john.doe@domain.com')
+                self.assertIsInstance(owner, Owner)
+
