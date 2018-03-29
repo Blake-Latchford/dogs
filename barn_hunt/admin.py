bd5891bb82b92d4ac4bd0ad36fa9e4f3eb5a405e
Blake Latchford <blake.latchford@gmail.com>
2018-03-28 20:17:59 -0400

    Rework admin page to simplify interface.

--- barn_hunt/admin.py
+++ barn_hunt/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 
-from .models import Address, Dog, Event, Owner, Trial, TrialClass
+from .models import Address, Dog, Event, Owner, Trial, TrialClass, CompetitionClass
 
-admin.site.register(Owner, admin.ModelAdmin)
-admin.site.register(Dog, admin.ModelAdmin)
-admin.site.register(Event, admin.ModelAdmin)
-admin.site.register(Address, admin.ModelAdmin)
-admin.site.register(TrialClass, admin.ModelAdmin)
-admin.site.register(Trial, admin.ModelAdmin)
+class HiddenAdmin(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        return {}
+
+admin.site.register(Owner, HiddenAdmin)
+admin.site.register(Address, HiddenAdmin)
+admin.site.register(CompetitionClass, HiddenAdmin)
+admin.site.register(Trial, HiddenAdmin)
+
+admin.site.register(Event)
+admin.site.register(Dog)
+
+class TrialClassAdmin(admin.ModelAdmin):
+    model = TrialClass
+admin.site.register(TrialClass, TrialClassAdmin)
