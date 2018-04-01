94ad951d6dda143b9ef6c626baf90d4f14feea05
Blake Latchford <blake.latchford@gmail.com>
2018-04-01 15:16:21 -0400

    Add owner name to dog registration form.

--- barn_hunt/forms.py
+++ barn_hunt/forms.py
@@ -1,10 +1,22 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Dog, Owner
 
 class DogRegistrationForm(forms.ModelForm):
-    owner_full_name = forms.CharField(max_length=200)
-    owner_email = forms.EmailField()
+    owner_full_name = forms.CharField(
+        max_length=200,
+        required=False)
+    owner_email = forms.EmailField(required=False)
+    owner_id = forms.IntegerField(
+        widget=forms.HiddenInput,
+        required=False,
+    )
+
+    owner = forms.IntegerField(
+        widget=forms.HiddenInput,
+        required=False,
+    )
 
     class Meta:
         model = Dog
@@ -14,4 +26,34 @@ class DogRegistrationForm(forms.ModelForm):
             'breed',
             'height_in_inches',
             'bitch_in_season',
+            'owner',
         ]
+
+    def clean(self):
+        super().clean()
+
+        owner = Owner.objects.filter(
+            pk=self.cleaned_data.get('owner_id')).first()
+
+        if not owner:
+            owner = Owner.objects.filter(
+                full_name=self.cleaned_data.get('owner_full_name'),
+                email=self.cleaned_data.get('owner_email')).first()
+
+        if not owner:
+            if not self.cleaned_data.get('owner_full_name'):
+                self.add_error('owner_full_name',
+                    ValidationError('Field Required', code='required'))
+            if not self.cleaned_data.get('owner_email'):
+                self.add_error('owner_email',
+                    ValidationError('Field Required', code='required'))
+
+            if not self.errors:
+                owner = Owner.objects.create(
+                    full_name=self.cleaned_data.get('owner_full_name'),
+                    email=self.cleaned_data.get('owner_email'))
+
+        self.cleaned_data['owner'] = owner
+        if owner:
+            self.cleaned_data['owner_full_name'] = owner.full_name
+            self.cleaned_data['owner_email'] = owner.email
--- barn_hunt/test_forms.py
+++ barn_hunt/test_forms.py
@@ -1,14 +1,16 @@
 from django.test import TestCase
 
 from . import forms
+from . import models
 
 class DogRegistrationFormTests(TestCase):
-    def test_empty_form(self):
+    def test_empty(self):
         form = forms.DogRegistrationForm()
         self.assertFalse(form.is_bound)
         self.assertFalse(form.is_valid())
 
-    def test_basic_valid_form(self):
+    def test_basic_valid(self):
+        initial_owner_count = models.Owner.objects.count()
         form = forms.DogRegistrationForm({
             'owner_full_name': 'John Doe',
             'owner_email': 'john.doe@domain.com',
@@ -18,10 +20,17 @@ class DogRegistrationFormTests(TestCase):
             'height_in_inches': 7,
             'bitch_in_season': False,
         })
-        self.assertTrue(form.is_bound)
-        self.assertTrue(form.is_valid(), form.errors)
 
-    def test_incomplete_form(self):
+        self.assertTrue(form.is_bound)
+        self.assertTrue(form.is_valid(), form.errors.as_text())
+
+        new_dog = form.save()
+
+        self.assertIsInstance(new_dog, models.Dog)
+        self.assertEqual(initial_owner_count + 1,
+            models.Owner.objects.count())
+
+    def test_incomplete(self):
         form = forms.DogRegistrationForm({
             'owner_full_name': 'John Doe',
             'owner_email': 'john.doe@domain.com',
@@ -31,6 +40,49 @@ class DogRegistrationFormTests(TestCase):
             #'height_in_inches': 7,
             'bitch_in_season': False,
         })
+
         self.assertTrue(form.is_bound)
         self.assertFalse(form.is_valid())
         self.assertIn('height_in_inches', form.errors)
+
+        with self.assertRaises(ValueError):
+            form.save()
+
+    def test_existing_owner(self):
+        owner = models.Owner.objects.create(
+            full_name='John Doe',
+            email='john.doe@domain.com',
+        )
+        initial_owner_count = models.Owner.objects.count()
+
+        form = forms.DogRegistrationForm({
+            'owner_id': owner.id,
+            'call_name': 'Meriln',
+            'registered_name': 'Pi R Squared',
+            'breed': 'Border Terrier',
+            'height_in_inches': 7,
+            'bitch_in_season': False,
+        })
+
+        self.assertTrue(form.is_bound)
+        self.assertTrue(form.is_valid(), form.errors.as_text())
+
+        new_dog = form.save()
+        self.assertIsInstance(new_dog, models.Dog)
+        self.assertEqual(initial_owner_count,
+            models.Owner.objects.count())
+
+    def test_unspecified_owner(self):
+        form = forms.DogRegistrationForm({
+            'call_name': 'Meriln',
+            'registered_name': 'Pi R Squared',
+            'breed': 'Border Terrier',
+            'height_in_inches': 7,
+            'bitch_in_season': False,
+        })
+
+        self.assertTrue(form.is_bound)
+        self.assertFalse(form.is_valid())
+
+        with self.assertRaises(ValueError):
+            new_dog = form.save()
